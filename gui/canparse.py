b0b19adda79abd1a3a48585a4a062ad4afe52bbe
Eric Evenchick <eric.evenchick@gmail.com>
2010-11-23 05:45:41 -0500

    changed to use configuration files, added next page button

--- gui/canparse.py
+++ gui/canparse.py
@@ -3,7 +3,7 @@ import os
 def get_key(msg): 
 	# split the message into parts
 	data = msg.split('-')
-	# mask the key bits (bits 1-2)
+	# mask the key bits (byte 1, bits 1-2)
 	key = int(data[2], 16) & 0x3
 	if key == 0:
 		return "Off"
@@ -18,7 +18,7 @@ def get_key(msg):
 
 def get_soc(msg):
 	data = msg.split('-')
-	# get SOC byte
+	# get SOC byte (byte 2)
 	soc = int(data[3], 16) & 0xFF
 	# scale
 	soc = soc/2
@@ -26,10 +26,23 @@ def get_soc(msg):
 
 def get_hvil(msg):
 	data = msg.split('-')
-	# mask the HVIL bit (bit 3)
+	# mask the HVIL bit (byte 1,bit 3)
 	hvil = int(data[2], 16) & 0x4
 	if hvil > 0:
 		return "On"
 	else:
 		return "Off"
+def get_h2_alarm(msg):
+	data = msg.split('-')
+	# get H2 alarm bit (byte 1, bit 4)
+	status = int(data[2], 16) & 0x8
+	if status > 0:
+		return True
+	else:
+		return False
 
+def get_temp(msg):
+	data = msg.split('-')
+	# get temp byte (byte 3)
+	status = int(data[4], 16) & 0xFF
+	return status
